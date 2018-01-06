55a952228bb2bdd37155f9d150e0321fefb2148d
hyt <ythuang2012163@163.com>
2018-01-06 16:25:56 +0800

    Finish some function

--- js/app.js
+++ js/app.js
@@ -19,12 +19,7 @@ let cards = [
     '<li class="card"><i class="fa fa-paper-plane-o"></i></li>',
     '<li class="card"><i class="fa fa-cube"></i></li>'
 ];
-/*
- * 显示页面上的卡片
- *   - 使用下面提供的 “shuffle” 方法对列表中的卡片进行洗牌
- *   - 循环遍历每张卡片，创建其 HTML
- *   - 将每张卡的 HTML 添加到页面
- */
+
 
 // 洗牌函数来自于 http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -40,26 +35,221 @@ function shuffle(array) {
 
     return array;
 }
+
 var newCards = shuffle(cards);
 
 
-// TODO:循环遍历每张卡片，创建其 HTML
+// 循环遍历每张卡片，创建其 HTML,将每张卡的 HTML 添加到页面
 for (let i = 0, len = newCards.length; i < len; i++) {
 	let card = $(newCards[i]);
 	$('.deck').append(card);
 }
 
-/*
- * 设置一张卡片的事件监听器。 如果该卡片被点击：
- *  - 显示卡片的符号（将这个功能放在你从这个函数中调用的另一个函数中）
- *  - 将卡片添加到状态为 “open” 的 *列表* 中（将这个功能放在你从这个函数中调用的另一个函数中）
- *  - 如果列表中已有另一张卡，请检查两张卡片是否匹配
- *    + 如果卡片匹配，将卡片锁定为 ”open“ 状态（将这个功能放在你从这个函数中调用的另一个函数中）
- *    + 如果卡片不匹配，请将卡片从列表中移除并隐藏卡片的符号（将这个功能放在你从这个函数中调用的另一个函数中）
- *    + 增加移动计数器并将其显示在页面上（将这个功能放在你从这个函数中调用的另一个函数中）
- *    + 如果所有卡都匹配，则显示带有最终分数的消息 将这个功能放在你从这个函数中调用的另一个函数中）
- */
+
+let steps = 0;
+let times = 0;
+const TWOSTAR = 15;
+const ONESTAR = 30;
+
+$('.card').on('click',function(){
+	$(this).addClass('open show');
+	addStep();
+	checkCard();
+
+});
+
 /**
- * @description
- * @param {n} [varname] [description]
+ * @description 增加计时器
  */
+
+$('.deck').one('click',function(){
+	$('body').everyTime('1s','A',function(){
+		times++;
+		$('.times').text(times);
+	});
+});
+
+/**
+ * @description 计算点击次数，以及判断星级
+ */
+function addStep(){
+	steps++;
+	$('.moves').text(steps);
+	if(steps > TWOSTAR){
+		$('.fa-star').first().css('opacity','0');
+	}
+	if(steps > ONESTAR){
+		$('.fa-star').eq(1).css('opacity','0');
+	}
+}
+
+/**
+ * @description 判断点击的卡片是否匹配
+ */
+function checkCard(){
+	let item1,item2;
+	if($('.open').length === 2){
+		item1 = $('.open').children()[0];
+		item2 = $('.open').children()[1];
+		item1.className === item2.className ? $('.open').toggleClass('match open show') : setTimeout(function(){$('.open').toggleClass('open show');},1200);
+	}
+	if($('.match').length === 16){
+		$('body').stopTime ('A');
+		$('.win').css('display','block');
+		$('.totalTime').text(times);
+		$('.totalStep').text(steps);
+
+		$('.again').on('click',function(){
+			window.location.reload();
+		});
+	}
+}
+
+/**
+ * @description 重新加载页面逻辑
+ */
+
+$('.restart').on('click',function(){
+	window.location.reload();
+});
+
+// jquery timers
+
+/**
+ * jQuery.timers - Timer abstractions for jQuery
+ * Written by Blair Mitchelmore (blair DOT mitchelmore AT gmail DOT com)
+ * Licensed under the WTFPL (http://sam.zoy.org/wtfpl/).
+ * Date: 2009/10/16
+ *
+ * @author Blair Mitchelmore
+ * @version 1.2
+ *
+ **/
+
+jQuery.fn.extend({
+    everyTime: function(interval, label, fn, times) {
+        return this.each(function() {
+            jQuery.timer.add(this, interval, label, fn, times);
+        });
+    },
+    oneTime: function(interval, label, fn) {
+        return this.each(function() {
+            jQuery.timer.add(this, interval, label, fn, 1);
+        });
+    },
+    stopTime: function(label, fn) {
+        return this.each(function() {
+            jQuery.timer.remove(this, label, fn);
+        });
+    }
+});
+
+jQuery.extend({
+    timer: {
+        global: [],
+        guid: 1,
+        dataKey: "jQuery.timer",
+        regex: /^([0-9]+(?:\.[0-9]*)?)\s*(.*s)?$/,
+        powers: {
+            // Yeah this is major overkill...
+            'ms': 1,
+            'cs': 10,
+            'ds': 100,
+            's': 1000,
+            'das': 10000,
+            'hs': 100000,
+            'ks': 1000000
+        },
+        timeParse: function(value) {
+            if (value == undefined || value == null)
+                return null;
+            var result = this.regex.exec(jQuery.trim(value.toString()));
+            if (result[2]) {
+                var num = parseFloat(result[1]);
+                var mult = this.powers[result[2]] || 1;
+                return num * mult;
+            } else {
+                return value;
+            }
+        },
+        add: function(element, interval, label, fn, times) {
+            var counter = 0;
+
+            if (jQuery.isFunction(label)) {
+                if (!times)
+                    times = fn;
+                fn = label;
+                label = interval;
+            }
+
+            interval = jQuery.timer.timeParse(interval);
+
+            if (typeof interval != 'number' || isNaN(interval) || interval < 0)
+                return;
+
+            if (typeof times != 'number' || isNaN(times) || times < 0)
+                times = 0;
+
+            times = times || 0;
+
+            var timers = jQuery.data(element, this.dataKey) || jQuery.data(element, this.dataKey, {});
+
+            if (!timers[label])
+                timers[label] = {};
+
+            fn.timerID = fn.timerID || this.guid++;
+
+            var handler = function() {
+                if ((++counter > times && times !== 0) || fn.call(element, counter) === false)
+                    jQuery.timer.remove(element, label, fn);
+            };
+
+            handler.timerID = fn.timerID;
+
+            if (!timers[label][fn.timerID])
+                timers[label][fn.timerID] = window.setInterval(handler,interval);
+
+            this.global.push( element );
+
+        },
+        remove: function(element, label, fn) {
+            var timers = jQuery.data(element, this.dataKey), ret;
+
+            if ( timers ) {
+
+                if (!label) {
+                    for ( label in timers )
+                        this.remove(element, label, fn);
+                } else if ( timers[label] ) {
+                    if ( fn ) {
+                        if ( fn.timerID ) {
+                            window.clearInterval(timers[label][fn.timerID]);
+                            delete timers[label][fn.timerID];
+                        }
+                    } else {
+                        for ( var fn in timers[label] ) {
+                            window.clearInterval(timers[label][fn]);
+                            delete timers[label][fn];
+                        }
+                    }
+
+                    for ( ret in timers[label] ) break;
+                    if ( !ret ) {
+                        ret = null;
+                        delete timers[label];
+                    }
+                }
+
+                for ( ret in timers ) break;
+                if ( !ret )
+                    jQuery.removeData(element, this.dataKey);
+            }
+        }
+    }
+});
+
+jQuery(window).bind("unload", function() {
+    jQuery.each(jQuery.timer.global, function(index, item) {
+        jQuery.timer.remove(item);
+    });
+});
